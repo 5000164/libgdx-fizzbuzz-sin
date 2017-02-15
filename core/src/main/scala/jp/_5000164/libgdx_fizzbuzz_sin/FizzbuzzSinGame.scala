1a0cd22aef269a9bd5725480b94e34b77650a923
Hiroshi Sugawara <5000164@users.noreply.github.com>
2017-02-16 00:44:58 +0900

    sin 波状に点を表示

--- core/src/main/scala/jp/_5000164/libgdx_fizzbuzz_sin/FizzbuzzSinGame.scala
+++ core/src/main/scala/jp/_5000164/libgdx_fizzbuzz_sin/FizzbuzzSinGame.scala
@@ -1,7 +1,9 @@
 package jp._5000164.libgdx_fizzbuzz_sin
 
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.graphics.{GL20, OrthographicCamera}
 import com.badlogic.gdx.{Game, Gdx, Screen}
+import scala.math.sin
 
 class FizzbuzzSinGame extends Game {
   override def create() {
@@ -12,12 +14,27 @@ class FizzbuzzSinGame extends Game {
 class TitleScreen(game: FizzbuzzSinGame) extends Screen {
   lazy val camera = new OrthographicCamera()
   camera.setToOrtho(false, 800, 480)
+  lazy val shapeRenderer = new ShapeRenderer()
 
   override def render(delta: Float) {
     Gdx.gl.glClearColor(0, 0, 0, 1)
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
     camera.update()
+
+    shapeRenderer.setProjectionMatrix(camera.combined)
+    shapeRenderer.begin(ShapeRenderer.ShapeType.Filled)
+
+    var base_y = 0.0
+
+    for (i <- 1 to 100) {
+      val x = i * 10
+      val y = ((sin(base_y) * 200) + 240).toInt
+      shapeRenderer.rect(x.toFloat, y.toFloat, 4f, 4f)
+      base_y += 0.25
+    }
+
+    shapeRenderer.end()
   }
 
 
